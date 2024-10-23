3e55daeb87455ad24e46c9a80efc2cd5ad0d311a
Mikolaj Karwowski <mikkarwowski@gmail.com>
2024-10-23 17:33:38 +0200

    Second master commit

--- app/src/main/java/com/example/gitrepoapp/MainActivity.kt
+++ app/src/main/java/com/example/gitrepoapp/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // I am doing stuff here that is important
+        // Very important
     }
 }
